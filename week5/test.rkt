e58d1ec97e63b8e90d9f833a4285fb3bdf3d32e4
Kostas Blatsoukas <k.blatsoukas@jrtechnologies.com>
2018-05-15 16:53:14 +0300

    Add racket part 2 started

---
+++ week5/test.rkt
@@ -0,0 +1,45 @@
+#lang racket
+
+(provide (all-defined-out)); this is for making everything public
+
+(define s "hello") ; variable that holds "hello"
+
+(define x 3)
+(define y (+ x 2)) ;this is a function call, in that case + is the function
+
+(define cube1
+  (lambda (x)
+         (* x (* x x)))) ;function definition, lamda is like anonymous function
+
+(define cube2
+  (lambda (x)
+         (* x x x))) ;function definition, lamda is like anonymous function
+
+(define (cube3 x)
+         (* x x x)) ;function definition, lamda is like anonymous function
+
+(define (pow1 x y)
+  (if (= y 0)
+  1
+  (* x (pow1 x (- y 1)))))
+
+(define pow2 ;curried version
+  (lambda (x)
+    (lambda (y)
+      (pow1 x y))))
+
+;racket lists
+;empty list: null
+;cons constructor: cons
+;head of the list: car
+;(list e1 ... en)
+
+
+(define (sum xs)
+  (if (null? xs)
+      0
+      (+ (car xs) (sum (cdr xs)))))
+
+
+
+
